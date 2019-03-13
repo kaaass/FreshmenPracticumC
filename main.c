f02a3388a8841079c9d109a9d4c8e13aeaa70e2d
KAAAsS <admin@kaaass.net>
2019-03-13 21:41:34 +0800

    初步实现链表

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "core/Database.h"
+#include "core/TableTest.h"
+
+int main() {
+    Database *db = Create(Test);
+    Test data[] = {{1, 233, "test"},
+                    {2, 666, "test2"}};
+    for (int i = 0; i < 2; i++) {
+        Database_pushBack(db, &data[i]);
+    }
+    ForEach(cur, db) {
+        Test *record = GetData(Test, cur);
+        printf("Id %d: Num = %d, Str = %s.\n", record->id, record->num, record->str);
+    }
+    return 0;
+}
