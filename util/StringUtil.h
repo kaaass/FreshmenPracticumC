c12623a143d24fe2d3b9f9fa138b2d8bf68da7a0
KAAAsS <admin@kaaass.net>
2019-03-18 14:50:30 +0800

    创建字符串Util

---
+++ util/StringUtil.h
@@ -0,0 +1,11 @@
+//
+// Created by guyis on 2019/3/18.
+//
+
+#ifndef FRESHMAN_PROJ_C_STRINGUTIL_H
+#define FRESHMAN_PROJ_C_STRINGUTIL_H
+
+#define LITERAL(str) str
+#define stringbuf char*;
+
+#endif //FRESHMAN_PROJ_C_STRINGUTIL_H
