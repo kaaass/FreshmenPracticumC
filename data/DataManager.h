1fcd569665a154e09998bb2c03874fea3a2b5a7f
KAAAsS <admin@kaaass.net>
2019-03-19 09:40:58 +0800

    增加DataManager模块

---
+++ data/DataManager.h
@@ -0,0 +1,13 @@
+//
+// Created by guyis on 2019/3/19.
+//
+#include "../core/Database.h"
+
+#ifndef FRESHMAN_PROJ_C_DATAMANAGER_H
+#define FRESHMAN_PROJ_C_DATAMANAGER_H
+
+Database *CONFIG;
+
+void DataManager_init();
+
+#endif //FRESHMAN_PROJ_C_DATAMANAGER_H
