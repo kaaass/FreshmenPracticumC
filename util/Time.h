0f8f42ae09f9a3c12e038a6d49e51a27e999a759
KevinAxel <2438679008@qq.com>
2019-03-19 09:17:28 +0800

    增加时间结构体

---
+++ util/Time.h
@@ -0,0 +1,17 @@
+//
+// Created by Kevin on 2019/3/19.
+//
+#include <time.h>
+
+#ifndef FRESHMAN_PROJ_C_TIME_H
+#define FRESHMAN_PROJ_C_TIME_H
+
+typedef struct {
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+} Time;
+
+#endif //FRESHMAN_PROJ_C_TIME_H
