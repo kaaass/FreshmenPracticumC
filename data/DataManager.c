52d66a759aa4901f0060067aa8649127034751d3
xuanlaoyee <1115810634@qq.com>
2019-03-19 10:13:20 +0800

    增加DataOrder

--- data/DataManager.c
+++ data/DataManager.c
@@ -4,7 +4,11 @@
 
 #include "../data/TableConfig.h"
 #include "DataManager.h"
+#include"TableOrder.h"
+#include"TableProvider.h"
 
 void DataManager_init() {
     CONFIG = Create(Config);
+    ORDER  = Create(Order);
+    PROVIDER = Create(Provider);
 }
