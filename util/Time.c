7ac2bf0a936fdcacff2f66d4f0968b3590790353
KAAAsS <admin@kaaass.net>
2019-03-20 00:13:22 +0800

    修复Time相关函数，增加单元测试

---
+++ util/Time.c
@@ -0,0 +1,50 @@
+//
+// Created by guyis on 2019/3/19.
+//
+#include "Time.h"
+
+/**
+ * 将时间戳解析为Time结构体
+ * @param now 待解析时间戳
+ * @return Time结构体
+ */
+Time Time_parseTime(time_t now) {
+    Time result = {
+            .month = -1,
+            .day = -1,
+            .hour = -1,
+            .minute = -1,
+            .second = -1,
+            .timeStamp = -1
+    };
+    if (now == -1) {
+        return result;
+    }
+    struct tm *data = localtime(&now);
+    result.month = data->tm_mon + 1;
+    result.day = data->tm_mday;
+    result.hour = data->tm_hour;
+    result.minute = data->tm_min;
+    result.second = data->tm_sec;
+    result.timeStamp = now;
+    return result;
+}
+
+/**
+ * 获取当前时间结构体
+ * @return Time结构体
+ */
+Time Time_getNow() {
+    time_t now = time(NULL);
+    return Time_parseTime(now);
+}
+
+/**
+ * 将时间结构体转为字符串形式
+ * @param structTime
+ * @return
+ */
+stringbuf Time_toLocalString(Time structTime) {
+    // TODO: 修正为要求的格式
+    return asctime(localtime(&structTime.timeStamp));
+}
--- util/Time.h
+++ util/Time.h
@@ -4,69 +4,26 @@
 #include <time.h>
 #include <stdio.h>
 
+#include "StringUtil.h"
+
 #ifndef FRESHMAN_PROJ_C_TIME_H
 #define FRESHMAN_PROJ_C_TIME_H
 //time_t now=time(NULL);
 
+/**
+ * 结构体：时间
+ */
 typedef struct {
     int month;
     int day;
     int hour;
     int minute;
     int second;
-    int timeStamp;
+    time_t timeStamp;
 } Time;
 
-
-Time Time_parseTime(time_t now)
-{
-    static Time timel;
-    now=time(NULL);
-    if(now==NULL)
-    {
-        printf("录入时间错误\n");
-        return;
-    }
-    struct tm Date=localtime(&now);
-    timel.month=Date.tm_mon+1;
-    timel.day=Date.tm_mday;
-    timel.hour=Date.tm_hour;
-    timel.minute=Date.tm_min;
-    timel.second=Date.tm_sec;
-    timel.timeStamp=(int)now-1546272000;
-    return timel;
-}
-
-
-
-
-
-/*void Time_parseTime(time_t now,Time time);//此函数不直接使用
-void Time_getNow(Time TIME);//将时间结构体调入
-
-
-
-
-void Time_getNow(Time TIME)
-{
-    time_t now=time(NULL);
-    return Time_parseTime(now,TIME);
-}
-void Time_parseTime(time_t now,Time *time)
-{
-    if(now==NULL)
-    {
-        printf("录入时间错误\n");
-        return;
-    }
-    struct tm Date=localtime(&now);
-    *time.month=Date.tm_mon+1;
-    *time.day=Date.tm_mday;
-    *time.hour=Date.tm_hour;
-    *time.minute=Date.tm_min;
-    *time.second=Date.tm_sec;
-    *time.timeStamp=(int)now-1546272000;
-}*/
-
+Time Time_parseTime(time_t now);
+Time Time_getNow();
+stringbuf Time_toLocalString(Time structTime);
 
 #endif //FRESHMAN_PROJ_C_TIME_H
