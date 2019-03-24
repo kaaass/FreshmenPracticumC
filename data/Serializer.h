2a316e7fe5a6401dce51eebdcc7caf2869f138a1
KevinAxel <2438679008@qq.com>
2019-03-24 17:10:03 +0800

    增加TableGuest的序列化反序列化

--- data/Serializer.h
+++ data/Serializer.h
@@ -3,6 +3,7 @@
 //
 #include "../util/StringUtil.h"
 #include "../util/Time.h"
+#include "../data/TableGuest.h"
 #include "cJSON.h"
 
 #ifndef FRESHMAN_PROJ_C_SERIALIZER_H
@@ -15,10 +16,12 @@
  * 序列化方法
  */
 cJSON *serialize_Time(Time);
+cJSON *serialize_Guest(Guest);
 
 /*
  * 反序列化方法
  */
 Time deserialize_Time(const cJSON *);
+Guest deseriablize_Guest(const cJSON *);
 
 #endif //FRESHMAN_PROJ_C_SERIALIZER_H
