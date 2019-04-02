16f021351ebf31a0c4efdf3b797f8ed9697a2f0b
KAAAsS <admin@kaaass.net>
2019-04-02 09:35:33 +0800

    增加了部分插入函数

---
+++ model/Insert.h
@@ -0,0 +1,14 @@
+//
+// Created by guyis on 2019/3/31.
+//
+#include "../util/StringUtil.h"
+
+#ifndef FRESHMAN_PROJ_C_INSERT_H
+#define FRESHMAN_PROJ_C_INSERT_H
+
+bool Insert_hasGuest(string name, string phone);
+void Insert_guest(string name, string phone);
+bool Insert_hasProvider(string name, string phone);
+void Insert_provider(string name, string phone);
+
+#endif //FRESHMAN_PROJ_C_INSERT_H
