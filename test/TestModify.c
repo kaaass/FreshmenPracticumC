cf6a1284e055f6e6d2a07d5aa4f2c765f3a4ade5
KevinAxel <2438679008@qq.com>
2019-04-08 14:57:29 +0800

    补全Modify的测试

--- test/TestModify.c
+++ test/TestModify.c
@@ -6,8 +6,21 @@
 
 #include "../data/DataManager.h"
 #include "../util/StringUtil.h"
+#include "../model/Modify.h"
+#include "../core/Database.h"
+#include "../data/TableOrder.h"
 
 void test_func() {
+    PurchaseRecord *pr = GetById(PurchaseRecord, PURCHASE_RECORD, 1);
+    SellingRecord *sr = GetById(SellingRecord, SELLING_RECORD, 1);
+    Order *order = GetById(Order, ORDER, 1);
+    Order *order2 = GetById(Order, ORDER, 3);
+    int opId[MAX_OP_ID] = {1, 2, 3, 4};
+    int opCount = 4;
+    TEST_ASSERT(deletePurchaseRecord(pr))
+    TEST_ASSERT(deleteSellingRecord(sr))
+    TEST_ASSERT(deleteOrder(order))
+    TEST_ASSERT(modifyOrder(order2, opId, opCount, 233.33))
 }
 
 int main() {
