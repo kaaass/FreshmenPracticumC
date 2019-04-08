59e3e531b58fd25ddc407b55a52e93007354bbe9
KAAAsS <admin@kaaass.net>
2019-04-08 13:05:15 +0800

    添加了Insert相关的测试

--- model/Insert.h
+++ model/Insert.h
@@ -2,6 +2,9 @@
 // Created by guyis on 2019/3/31.
 //
 #include "../util/StringUtil.h"
+#include "../data/TablePurchaseRecord.h"
+#include "../data/TableSellingRecord.h"
+#include "../data/TableOrder.h"
 
 #ifndef FRESHMAN_PROJ_C_INSERT_H
 #define FRESHMAN_PROJ_C_INSERT_H
@@ -10,5 +13,8 @@ bool Insert_hasGuest(string name, string phone);
 void Insert_guest(string name, string phone);
 bool Insert_hasProvider(string name, string phone);
 void Insert_provider(string name, string phone);
+void Insert_sellingRecord(SellingRecord *);
+void Insert_purchaseRecord(PurchaseRecord *);
+void Insert_order(Order *);
 
 #endif //FRESHMAN_PROJ_C_INSERT_H
