35b3f60205087aee2c99d5666c86f12c0f470f2c
xuanlaoyee <1115810634@qq.com>
2019-04-09 10:18:53 +0800

    为Statistics添加进货价格点、售卖价格点两个函数

--- model/Statistics.c
+++ model/Statistics.c
@@ -14,8 +14,8 @@
 #include "Statistics.h"
 #define type_num 5//零部件的种类数
 
-#define DATA_TYPE_Report 10
-#define DATA_TYPE_Any_gift 11
+#define DATA_TYPE_PurchasePot 10
+#define DATA_TYPE_SellPot 11
 #define DATA_TYPE_Present_Situation 12
 
 int compareGift_situation (const void * a, const void * b){
@@ -155,3 +155,39 @@ Database * Print_Purchaserecord(Time a,Time b){
     }
     return arrayToDatabase(Data(PurchaseRecord, temp1),ii);
 }//按照时间顺序查找购买记录
+
+Database * SellScatter(enum MountingsType type_scan){
+    Database * SELLSCATTER=NULL;
+    ForEach(cur,SELLING_RECORD){
+        SellingRecord *record = GetData(SellingRecord, cur);
+        Mountings *rec_mountings = GetById(Mountings,MOUNTINGS,record->partId);
+        if(type_scan==rec_mountings->type&&(record->status==0||record->status==2||record->status==3))
+        {
+            SellPot temp={
+                    .type=rec_mountings->type,
+                    .price=record->price,
+                    .time_pot=record->time
+            };
+            Database_pushBack(SELLSCATTER,Data(SellPot,&temp));
+        }
+    }
+    return SELLSCATTER;
+}////这是得到某货物时间与售价 点
+
+Database * PurchaseScatter(enum MountingsType type_scan){
+    Database * PURCHASESCATTER=NULL;
+    ForEach(cur,PURCHASE_RECORD){
+        PurchaseRecord *record = GetData(PurchaseRecord, cur);
+        Mountings *rec_mountings = GetById(Mountings,MOUNTINGS,record->partId);
+        if(type_scan==rec_mountings->type&&(record->status==0||record->status==2))
+        {
+            PurchasePot temp={
+                    .type=rec_mountings->type,
+                    .price=record->price,
+                    .time_pot=record->time
+            };
+            Database_pushBack(PURCHASESCATTER,Data(PurchasePot,&temp));
+        }
+    }
+    return PURCHASESCATTER;
+}////这是得到某货物时间与进价 点
