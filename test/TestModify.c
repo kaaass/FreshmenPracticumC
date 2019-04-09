984405fda9b5b8f427086fc7b1525e9a66079247
KevinAxel <2438679008@qq.com>
2019-04-09 08:22:06 +0800

    修改Modify的测试

--- test/TestModify.c
+++ test/TestModify.c
@@ -13,26 +13,24 @@
 void test_func() {
 
     TEST_ASSERT(deletePurchaseRecord(1))
-    TEST_ASSERT(((PurchaseRecord*)GetById(PurchaseRecord, PURCHASE_RECORD, 1))->status == PURCHASE_DELETED)
+    TEST_ASSERT(GetById(PurchaseRecord, PURCHASE_RECORD, 1)->status == PURCHASE_DELETED)
 
     TEST_ASSERT(deleteSellingRecord(1))
-    TEST_ASSERT(((SellingRecord*)GetById(SellingRecord, SELLING_RECORD, 1))->status == SELLING_DELETED)
+    TEST_ASSERT(GetById(SellingRecord, SELLING_RECORD, 1)->status == SELLING_DELETED)
 
     TEST_ASSERT(deleteOrder(1))
-    TEST_ASSERT(((Order*)GetById(Order, ORDER, 1))->status == ORDER_DELETED)
+    TEST_ASSERT(GetById(Order, ORDER, 1)->status == ORDER_DELETED)
 
     PurchaseRecord pr = {.price = 998244353};
     SellingRecord sr = {.price = 1e9 + 7};
-    Insert_purchaseRecord(&pr);
-    Insert_sellingRecord(&sr);
 
-    TEST_ASSERT(modifyOrderOfPurchaseRecord(1, 1, GetById(PurchaseRecord, PURCHASE_RECORD, Database_size(PURCHASE_RECORD))))
+    TEST_ASSERT(modifyOrderOfPurchaseRecord(1, 1, &pr))
     Order *order = GetById(Order, ORDER, Database_size(ORDER));
-    TEST_ASSERT(((PurchaseRecord*)GetById(PurchaseRecord, PURCHASE_RECORD, order->opId[1]))->price == pr.price)
+    TEST_ASSERT(GetById(PurchaseRecord, PURCHASE_RECORD, order->opId[order->opCount-1])->price == pr.price)
 
-    TEST_ASSERT(modifyOrderOfSellingRecord(3, 1, GetById(SellingRecord, SELLING_RECORD, Database_size(SELLING_RECORD))))
+    TEST_ASSERT(modifyOrderOfSellingRecord(3, 1, &sr))
     order = GetById(Order, ORDER, Database_size(ORDER));
-    TEST_ASSERT(((SellingRecord*)GetById(SellingRecord, SELLING_RECORD, order->opId[1]))->price == sr.price)
+    TEST_ASSERT(GetById(SellingRecord, SELLING_RECORD, order->opId[order->opCount-1])->price == sr.price)
 
 }
 
