3deeb9c5deb2a5be1ba45a67f6f911e622d0733e
KevinAxel <2438679008@qq.com>
2019-04-09 09:41:49 +0800

    增加double类型配置的set,get函数

--- core/Config.h
+++ core/Config.h
@@ -11,6 +11,8 @@ void Config_setString(string key, string value);
 stringbuf Config_optString(string key, string defVal);
 
 void Config_setInteger(string key, int value);
+void Config_setDouble(string key, double value);
 int Config_optInteger(string key, int defVal);
+double Config_optDouble(string key, double defVal);
 
 #endif //FRESHMAN_PROJ_C_CONFIG_H
