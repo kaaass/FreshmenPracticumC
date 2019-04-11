7f6203332cb5d1c886b53007052aa6c40a143a19
KAAAsS <admin@kaaass.net>
2019-04-12 01:03:56 +0800

    增加了浏览记录页

---
+++ ui/scene/View.c
@@ -0,0 +1,43 @@
+//
+// Created by guyis on 2019/4/11.
+//
+
+#include "View.h"
+#include "../UI.h"
+#include "../Table.h"
+
+#define TABLE_COLUNN_NUM 5
+
+Table *dataTable;
+
+void View_init() {
+    stringbuf columnName[] = {
+            STR_BUF("测试"),
+            STR_BUF("列A"),
+            STR_BUF("列B"),
+            STR_BUF("列C"),
+            STR_BUF("列D")
+    };
+    stringbuf testData[][TABLE_COLUNN_NUM] = {
+            {STR_BUF("1"), STR_BUF("测试"), STR_BUF("测试"), STR_BUF("66666"), STR_BUF("测试")},
+            {STR_BUF("2"), STR_BUF("哈哈哈哈"), STR_BUF("测试"), STR_BUF("测试"), STR_BUF("测试")},
+            {STR_BUF("3"), STR_BUF("测试"), STR_BUF("233333"), STR_BUF("测试"), STR_BUF("测试")},
+            {STR_BUF("4"), STR_BUF("你写"), STR_BUF("代码"), STR_BUF("像"), STR_BUF("蔡徐坤")},
+            {STR_BUF("5"), STR_BUF("这表格"), STR_BUF("写的"), STR_BUF("累死"), STR_BUF("我了")}
+    };
+    int columnWidth[] = {10, 21, 21, 21, 21};
+    READ_SPEC = true;
+    dataTable = Table_create(-1, 1, 100, 20, 1);
+    Table_setColumnTitle(dataTable, columnName, columnWidth, TABLE_COLUNN_NUM);
+    for (int i = 0; i < 5; i++) Table_pushLine(dataTable, testData[i]);
+    UI_setFooterUpdate(LITERAL(""));
+    UI_startScene(SCENE_VIEW, STR_BUF("浏览记录"));
+}
+
+void View_inLoop() {
+    Table_inLoop(dataTable);
+}
+
+int View_render(int line) {
+    Table_render(dataTable, line);
+}
