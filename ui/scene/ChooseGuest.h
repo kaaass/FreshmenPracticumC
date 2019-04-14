2d909edf8df140ff66363ddb399fb23758b608ea
KAAAsS <admin@kaaass.net>
2019-04-15 00:02:25 +0800

    增加客户增加页面，完成页面设计

---
+++ ui/scene/ChooseGuest.h
@@ -0,0 +1,21 @@
+//
+// Created by guyis on 2019/4/14.
+//
+
+#ifndef FRESHMAN_PROJ_C_CHOOSEGUEST_H
+#define FRESHMAN_PROJ_C_CHOOSEGUEST_H
+
+#include "../../data/TableGuest.h"
+
+#define SCENE_CHOOSE_GUEST 6
+
+extern int GUEST_ID;
+extern Guest CUR_GUEST;
+
+void ChooseGuest_init(int guestId, int type);
+
+void ChooseGuest_inLoop();
+
+int ChooseGuest_render(int);
+
+#endif //FRESHMAN_PROJ_C_CHOOSEGUEST_H
