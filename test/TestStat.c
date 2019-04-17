c3c51decb67a2dd254f40d36f4298345fbd974ca
xuanlaoyee <1115810634@qq.com>
2019-04-17 20:14:28 +0800

    改了礼物页面

--- test/TestStat.c
+++ test/TestStat.c
@@ -11,6 +11,7 @@
 #include "../model/Statistics.h"
 #include "../util/Time.h"
 
+
 void test_statistics(){
     printf("%lf\n",Purchase_total());
     printf("%lf\n",Sell_total());
@@ -27,21 +28,9 @@ void test_gift_situation(){
     }
     Database_destroy(db);
 }
-
-void test_search_gift(){
-    Database *db=Search_gift(1);
-    ForEach(cur, db){
-        Present_Situation *now = GetData(Present_Situation, cur);
-        stringbuf timeStr = Time_toLocalString(now->time_recording);
-        printf("AMOUNT=%lf,Guest=%s,amount=%d,time=%s",now->AMOUNT,CSTR(now->people.phone),now->amount,CSTR(timeStr));
-        $STR_BUF(timeStr);
-    }
-    Database_destroy(db);
-}
-
 void test_time_search_gift(){
-        Time a = Time_parseTime(1553011043);
-        Time b = Time_parseTime(1553011344);
+    Time a = Time_parseTime(1553011043);
+    Time b = Time_parseTime(1553011344);
     Database *db=Print_Sellingrecord(a, b);
     ForEach(cur, db) {
         SellingRecord *now = GetData(SellingRecord, cur);
@@ -54,6 +43,27 @@ void test_time_search_gift(){
     }
     Database_destroy(db);
 }
+void test_search_gift(){
+    Database *db=Search_gift(2);
+    ForEach(cur, db){
+        Present_Situation *now = GetData(Present_Situation, cur);
+        stringbuf timeStr = Time_toLocalString(now->time_recording);
+        printf("AMOUNT=%lf,Guest=%s,amount=%d,time=%s",now->AMOUNT,CSTR(now->people.phone),now->amount,CSTR(timeStr));
+        $STR_BUF(timeStr);
+    }
+    Database_destroy(db);
+}
+
+void test_search_amount_gift(){
+    Database *db=Search_amount_gift();
+    ForEach(cur,db){
+        SingleGift *now = GetData(SingleGift,cur);
+       printf("name=%s,total=%.2lf,amount=%d",CSTR(now->name),now->total,now->amount);
+    }
+}
+void test_purchase_scatter(){
+   PurchaseScatter(1);
+}
 
 int main() {
     UNITY_BEGIN();
@@ -64,9 +74,10 @@ int main() {
 
     //RUN_TEST(test_statistics);
     //RUN_TEST(test_gift_situation);
-    RUN_TEST(test_search_gift);
+    //RUN_TEST(test_search_gift);
     //RUN_TEST(test_time_search_gift);
-
+    RUN_TEST(test_search_amount_gift);
+    //RUN_TEST(test_purchase_scatter);
     DataManager_finalize();
 
     return UNITY_END();
