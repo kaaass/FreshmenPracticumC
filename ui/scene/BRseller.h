8372641f752ad3ead51479186de26a876a7e8e73
soobright <d1564050317@163.com>
2019-04-19 17:35:01 +0800

    缝缝补补又一年

---
+++ ui/scene/BRseller.h
@@ -0,0 +1,16 @@
+//
+// Created by St on 2019-04-19.
+//
+
+#ifndef FRESHMAN_PROJ_C_BRSELLER_H
+#define FRESHMAN_PROJ_C_BRSELLER_H
+
+#define SCENE_BRSELLER 19
+
+void BRseller_init();
+
+void BRseller_inLoop();
+
+int BRseller_render(int );
+
+#endif //FRESHMAN_PROJ_C_BRSELLER_H
