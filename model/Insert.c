914f8398afd5a75fd1fef1bab00342ccb18e3dd1
KAAAsS <admin@kaaass.net>
2019-04-20 23:06:38 +0800

    价格依照时间变更

--- model/Insert.c
+++ model/Insert.c
@@ -85,6 +85,7 @@ void Insert_sellingRecord(SellingRecord *data, bool change) {
     if (change) {
         Mountings *mounting = GetById(Mountings, MOUNTINGS, data->partId);
         mounting->amount -= data->amount;
+        mounting->price = data->price;
     }
     Database_pushBack(SELLING_RECORD, Data(SellingRecord, data));
 }
@@ -100,6 +101,7 @@ void Insert_purchaseRecord(PurchaseRecord *data, bool change) {
     if (change) {
         Mountings *mounting = GetById(Mountings, MOUNTINGS, data->partId);
         mounting->amount += data->amount;
+        mounting->price = data->price;
     }
     Database_pushBack(PURCHASE_RECORD, Data(PurchaseRecord, data));
 }
