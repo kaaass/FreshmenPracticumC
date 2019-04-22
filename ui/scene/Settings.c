6da4faea1f1fc0def05595f183b42b723e9668b2
KAAAsS <admin@kaaass.net>
2019-04-22 21:27:37 +0800

    完善插入逻辑

--- ui/scene/Settings.c
+++ ui/scene/Settings.c
@@ -2,10 +2,14 @@
 // Created by guyis on 2019/4/21.
 //
 
+#include <cstring_jslike.h>
 #include "Settings.h"
+#include "../UI.h"
 
 void Settings_init() {
-
+    READ_SPEC = true;
+    UI_setFooterUpdate(LITERAL("按下Esc键以返回..."));
+    UI_startScene(SCENE_SETTINGS, STR_BUF("设置"));
 }
 
 void Settings_inLoop() {
