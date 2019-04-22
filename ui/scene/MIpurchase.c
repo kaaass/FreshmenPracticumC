aef91384dded4ce81791bc2f2c550502d2e199ff
soobright <d1564050317@163.com>
2019-04-22 16:37:14 +0800

    完善批量增加

--- ui/scene/MIpurchase.c
+++ ui/scene/MIpurchase.c
@@ -32,7 +32,7 @@ void MIpurchase_inLoop(){
         cJSON *json;
         string dir;
         dir = UI_inputString(LITERAL("请输入文件路径："));
-        stringbuf path, content;
+        stringbuf content;
         // 检测文件存在
         if (!isExist(CSTR(dir))) {
             string instruction = LITERAL("文件不存在，按Esc返回上一页面");
@@ -42,16 +42,17 @@ void MIpurchase_inLoop(){
         }
         // PurchaseRecord
         else{
-            path = LITERAL("/PurchaseRecord.json");
-            path = concat(2, dir, path);
-            content = readStringFromFile(CSTR(path));
+            content = readStringFromFile(CSTR(dir));
             json = cJSON_Parse(U8_CSTR(content));
             ForEach(cur,json){
                 PurchaseRecord *record = GetData(PurchaseRecord,cur);
                 Database_pushBack(PURCHASE_RECORD,Data(PurchaseRecord,record));
             }
             cJSON_Delete(json);
-            $STR_BUF(path);
+            string instruction = LITERAL("导入成功，按Esc返回上一页面");
+            UI_setFooterUpdate(instruction);
+            if(SPEC_KEY == KEY_ESC)
+                UI_endScene();
         }
         if(SPEC_KEY == KEY_ESC)
             UI_endScene();
